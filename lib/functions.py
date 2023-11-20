a6795a9e04205ddf933ed72ba4721d0f88d8e30a
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2023-11-21 00:28:22 +0300

    Assignment Completed

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    print(num1 + num2)
 
 def halve(number):
-    pass
+    if number is not int:
+        print("Null")
+    else:    
+        print(number/2)
